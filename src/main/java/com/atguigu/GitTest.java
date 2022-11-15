47f0108847d67088843f9ebf81053132cfbc09ce
FUCK <Fuck@qq.com>
2022-11-15 09:31:01 +0800

    master4

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -1,6 +1,4 @@
 package com.atguigu;
-
-
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git!!!!");
@@ -8,5 +6,6 @@ public class GitTest {
         System.out.println("三次修改");
         System.out.println("hot-fix");
         System.out.println("master3");
+        System.out.println("master4");
     }
 }
