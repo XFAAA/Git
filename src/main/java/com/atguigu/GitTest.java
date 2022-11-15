7a24f77088db824b04d1e5d3977d5dd17ac9b960
FUCK <Fuck@qq.com>
2022-11-15 09:29:46 +0800

    hot-fix4

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("三次修改");
         System.out.println("hot-fix2");
         System.out.println("hot-fix3");
+        System.out.println("hot-fix4");
     }
 }
